7cae7000e23f7c1e2c26734d917f01d66e15b54b
zszsrex <1152902354@qq.com>
2016-12-10 19:28:40 +0800

    Add files via upload

---
+++ javascripts/songs.js
@@ -0,0 +1,5 @@
+
+var songSrc=new Array();
+songSrc["spyair.mp3"]="http://v2.huangguofeng.com/pcs_download.php?id=31971&link=%2Fapps%2Fhgf_blog%2Fmusic%2Fspyair.MP3";
+songSrc["好想大声说喜欢你.mp3"]="http://v2.huangguofeng.com/pcs_download.php?id=31971&link=%2Fapps%2Fhgf_blog%2Fmusic%2FBAAD+%28%E3%83%90%E3%83%BC%E3%83%89%29+-+%E5%90%9B%E3%81%8C%E5%A5%BD%E3%81%8D%E3%81%A0%E3%81%A8%E5%8F%AB%E3%81%B3%E3%81%9F%E3%81%84+%28%E5%A5%BD%E6%83%B3%E5%A4%A7%E5%A3%B0%E8%AF%B4%E5%96%9C%E6%AC%A2%E4%BD%A0%29+%5Bmqms2%5D.mp3";
+songSrc["暧昧-王菲.mp3"]="http://v2.huangguofeng.com/pcs_download.php?id=31971&link=%2Fapps%2Fhgf_blog%2Fmusic%2F%E6%9A%A7%E6%98%A7.mp3";
